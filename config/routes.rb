97b6ecb64dcd203dd4a2c7e1f79e5eb439b52042
Duke <dukekhaos@gmail.com>
2015-11-07 21:54:34 -0200

    mount blazer at /

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   authenticate :user, lambda { |user| user } do
-    mount Blazer::Engine, at: "blazer"
+    mount Blazer::Engine, at: "/"
   end
 end
