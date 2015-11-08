e7e34abdac3ef76525d971a403e6a377d7481071
Duke <dukekhaos@gmail.com>
2015-11-08 00:20:32 -0200

    add email settings

---
+++ config/initializers/email_settings.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  address: ENV['SMTP_ADDRESS'],
+  port: ENV['SMTP_PORT'],
+  domain: "uhura.io",
+  user_name: "support@uhura.io",
+  password: ENV['SMTP_PASSWORD'],
+  authentication: "plain",
+  enable_starttls_auto: true
+}
