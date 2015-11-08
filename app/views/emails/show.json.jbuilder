114c509d712408195546da618301489c9dd35339
Duke <dukekhaos@gmail.com>
2015-11-08 13:28:58 -0200

    Revert "add emails scaffold"
    This reverts commit 0593310607477b3b7522d5d160f1f15967c3a6d8.

--- app/views/emails/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @email, :id, :title, :body, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108151113) do
+ActiveRecord::Schema.define(version: 20151018131159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,13 +82,6 @@ ActiveRecord::Schema.define(version: 20151108151113) do
     t.string   "data_source"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "emails", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
