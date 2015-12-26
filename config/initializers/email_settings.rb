38a83bdd1a3780ff6d7c25f643929103f4772a1d
Duke <dukekhaos@gmail.com>
2015-12-26 19:43:36 -0200

    config mailer

--- config/initializers/email_settings.rb
+++ config/initializers/email_settings.rb
@@ -1,15 +1,16 @@
-Rails.application.config.active_job.queue_adapter = :inline
+if Rails.env.production?
+  Rails.application.config.active_job.queue_adapter = :inline
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  address: ENV['SMTP_ADDRESS'],
-  port: ENV['SMTP_PORT'],
-  domain: ENV['SMTP_DOMAIN'],
-  user_name: ENV['SMTP_USERNAME'],
-  password: ENV['SMTP_PASSWORD'],
-  authentication: :login,
-  ssl: true,
-  tls: true,
-  enable_starttls_auto: true
-}
-
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: ENV['SMTP_ADDRESS'],
+    port: ENV['SMTP_PORT'],
+    domain: ENV['SMTP_DOMAIN'],
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: :login,
+    ssl: true,
+    tls: true,
+    enable_starttls_auto: true
+  }
+end
